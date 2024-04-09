5132a41b0fb7d331b9b6664442593b9a7a06dc81
coreybriscoe <coreybriscoe@gmail.com>
2024-04-09 14:18:29 -0400

    refactor: Move CardGame code to code-behind

---
+++ src/BlazorCardGame.WebUI/Components/Pages/CardGame.razor.cs
@@ -0,0 +1,123 @@
+namespace BlazorCardGame.WebUI.Components.Pages;
+
+public partial class CardGame
+{
+    private int handLimit = 8;
+    private void DeckClicked()
+    {
+        if (deck.Count() < 1 || hand.Count() >= handLimit) return;
+        ICard card = deck.Draw();
+        hand.Add((PlayingCard) card);
+    }
+
+    List<BasePlayingCard> hand = new List<BasePlayingCard>
+    {
+        // new PlayingCard(1, '♦', isFaceUp: true, isSelectable: true),
+    };
+
+    static Deck deck;
+
+    protected override void OnInitialized()
+    {
+        char[] suits = { '♦', '♣', '♥', '♠' };
+        List<ICard> deckCards = new List<ICard>();
+        for (int i = 2; i < 15; i++)
+        {
+            string rankString = i switch
+            {
+                11 => "J",
+                12 => "Q",
+                13 => "K",
+                14 => "A",
+                _ => i.ToString()
+            };
+            for (int j = 0; j < 4; j++)
+            {
+                deckCards.Add(new PlayingCard(i, suits[j], rankString: rankString, isFaceUp: true, isSelectable: true));
+            }
+        }
+        deck = new Deck(deckCards);
+        deck.Shuffle();
+
+        // Draw cards
+        for (int i = 0; i < handLimit; i++)
+        {
+            ICard card = deck.Draw();
+            hand.Add((PlayingCard) card);
+        }
+    }
+
+    private int anteLevel = 1;
+    private int roundLevel = 1;
+    private int phaseLevel = 1; // [1, 2, 3] for small, big, and boss blinds
+
+    private string handCategory = "?";
+    private int handScore = 0;
+    private int handMultiplier = 0;
+    private int roundHandsRemaining = 3;
+    private void OnHandCardsSelected(List<BasePlayingCard> handCards)
+    {
+        PokerLogic.HandCategory selectedCardsCategory = PokerLogic.GetHandCategory(handCards.ToList<IPlayingCard>());
+        handCategory = PokerLogic.HandCategoryNames.GetValueOrDefault(selectedCardsCategory, "?");
+        if (handCategory == "?") {
+            handScore = 0;
+            handMultiplier = 0;
+            return;
+        }
+        // Determine points: sum of scoring cards' value and base category points
+        int points = HandScores.handBasePoints.GetValueOrDefault<PokerLogic.HandCategory, int>(selectedCardsCategory, 0);
+        List<BasePlayingCard> selectedCards = handCards.Where(c => ((IPlayingCard) c).IsSelected()).ToList();
+        // Scoring cards are cards for which their exclusion would change the hand category
+        // unless the hand category is high card, in which case score only the highest card
+        if (selectedCardsCategory != PokerLogic.HandCategory.HighCard)
+        {
+            points += selectedCards.Where(
+                c => selectedCardsCategory != PokerLogic.GetHandCategory(selectedCards.Where(
+                    c2 => !c2.Equals(c)).ToList<IPlayingCard>())
+            ).ToList<BasePlayingCard>().Sum(c => c.GetPoints());
+        }
+        else
+        {
+            points += selectedCards.Max(c => c.GetPoints());
+        }
+        handScore = points;
+        handMultiplier = HandScores.handBaseMultiplier.GetValueOrDefault<PokerLogic.HandCategory, int>(selectedCardsCategory, 0);
+    }
+
+    private int roundScore = 0;
+    private void PlayHandClicked()
+    {
+        roundScore += handScore * handMultiplier;
+        // Replace selected cards
+        List<BasePlayingCard> selectedCards = hand.Where(c => ((IPlayingCard) c).IsSelected()).ToList<BasePlayingCard>();
+        hand.RemoveAll(c => ((IPlayingCard) c).IsSelected());
+        selectedCards.ForEach(c => {
+            c.ToggleSelect();
+            ICard replacementCard = deck.Draw();
+            hand.Add((PlayingCard) replacementCard);
+        });
+        // Update info area values
+        handCategory = "?";
+        handScore = 0;
+        handMultiplier = 0;
+        roundHandsRemaining--;
+    }
+
+    private int roundDiscardsRemaining = 2;
+    private void DiscardHandClicked()
+    {
+        // Replace selected cards
+        List<BasePlayingCard> selectedCards = hand.Where(c => ((IPlayingCard) c).IsSelected()).ToList<BasePlayingCard>();
+        hand.RemoveAll(c => ((IPlayingCard) c).IsSelected());
+        selectedCards.ForEach(c => {
+            c.ToggleSelect();
+            ICard replacementCard = deck.Draw();
+            hand.Add((PlayingCard) replacementCard);
+        });
+        // Update info area values
+        handCategory = "?";
+        handScore = 0;
+        handMultiplier = 0;
+        roundDiscardsRemaining--;
+    }
+}
