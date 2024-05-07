c55a868db1c20c4771c20c78362f2314481df916
coreybriscoe <coreybriscoe@gmail.com>
2024-05-06 22:28:55 -0400

    fix: Fix missing and broken play and discard buttons

--- src/BlazorCardGame.WebUI/Components/Pages/CardGame.razor.cs
+++ src/BlazorCardGame.WebUI/Components/Pages/CardGame.razor.cs
@@ -57,22 +57,57 @@ public partial class CardGame : FluxorComponent
         Dispatcher.Dispatch(new SetDiscardsRemainingAction(3));
     }
 
-    private string handCategory = "?";
-    private int handScore = 0;
-    private int handMultiplier = 0;
+    private PokerLogic.HandCategory selectedCardsCategory = PokerLogic.HandCategory.NoCategory;
+    private bool isPlayableHand()
+    {
+        return PokerLogic.GetHandCategory(CardsState.Value.HandCards.Where(c => ((IPlayingCard) c).IsSelected()).ToList()) != PokerLogic.HandCategory.NoCategory;
+    }
+
+    private long getHandScore()
+    {
+        List<BasePlayingCard> selectedCards = CardsState.Value.HandCards.Where(c => ((IPlayingCard) c).IsSelected()).ToList();
+        // Determine points: sum of scoring cards' value and base category points
+        long points = HandScores.handBasePoints.GetValueOrDefault<PokerLogic.HandCategory, int>(selectedCardsCategory, 0);
+        // Scoring cards are cards for which their exclusion would change the hand category
+        // unless the hand category is high card, in which case score only the highest card
+        if (selectedCardsCategory != PokerLogic.HandCategory.HighCard)
+        {
+            points += CardsState.Value.HandCards.Where(
+                c => selectedCardsCategory != PokerLogic.GetHandCategory(
+                    selectedCards.Where(
+                        c2 => !c2.Equals(c)
+                    ).ToList<IPlayingCard>()
+                )
+            ).Sum(c => c.GetPoints());
+        }
+        else
+        {
+            points += CardsState.Value.HandCards.Where(c => ((IPlayingCard) c).IsSelected()).Max(c => c.GetPoints());
+        }
+        return points;
+    }
+
+    private long getHandMultiplier()
+    {
+        return HandScores.handBaseMultiplier.GetValueOrDefault<PokerLogic.HandCategory, int>(selectedCardsCategory, 0);
+    }
 
     private void PlayHandClicked()
     {
-        Dispatcher.Dispatch(new AddRoundScoreAction(handScore * handMultiplier));
+        selectedCardsCategory = PokerLogic.GetHandCategory(CardsState.Value.HandCards.Where(c => ((IPlayingCard) c).IsSelected()).ToList());
+        Dispatcher.Dispatch(new AddRoundScoreAction(getHandScore() * getHandMultiplier()));
         // Replace selected cards
         List<BasePlayingCard> newHand = CardsState.Value.HandCards.ToList();
-        newHand.RemoveAll(c => ((IPlayingCard) c).IsSelected());
         List<BasePlayingCard> selectedCards = newHand.Where(c => ((IPlayingCard) c).IsSelected()).ToList<BasePlayingCard>();
         selectedCards.ForEach(c => {
             c.ToggleSelect();
+        });
+        newHand.RemoveAll(c => ((IPlayingCard) c).IsSelected());
+        while (newHand.Count < handLimit)
+        {
             ICard replacementCard = deck.Draw();
             newHand.Add((PlayingCard) replacementCard);
-        });
+        }
         Dispatcher.Dispatch(new SetHandCardsAction(newHand));
         Dispatcher.Dispatch(new DecrementHandsRemainingAction());
     }
@@ -82,11 +117,12 @@ public partial class CardGame : FluxorComponent
         // Replace selected cards
         List<BasePlayingCard> newHand = CardsState.Value.HandCards.ToList();
         newHand.RemoveAll(c => ((IPlayingCard) c).IsSelected());
-        newHand.ForEach(c => {
-            c.ToggleSelect();
+        while (newHand.Count < handLimit)
+        {
             ICard replacementCard = deck.Draw();
             newHand.Add((PlayingCard) replacementCard);
-        });
+        }
+        Dispatcher.Dispatch(new SetHandCardsAction(newHand));
         Dispatcher.Dispatch(new DecrementDiscardsRemainingAction());
     }
 }
