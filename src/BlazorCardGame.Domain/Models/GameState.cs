d9082ef004410be4cfb96d690bcc3719e46c8907
coreybriscoe <coreybriscoe@gmail.com>
2024-05-08 23:21:11 -0400

    feat: Add GameState access and mutate methods, constructor

--- src/BlazorCardGame.Domain/Models/GameState.cs
+++ src/BlazorCardGame.Domain/Models/GameState.cs
@@ -2,15 +2,40 @@ namespace BlazorCardGame.Domain.Models
 {
     public class GameState
     {
-        public List<ICard> Jokers { get; private set; } = [];
-        public List<ICard> Consumables { get; private set; } = [];
+        private List<ICard> Jokers { get; set; } = [];
+        private List<ICard> Consumables { get; set; } = [];
         public List<BasePlayingCard> HandCards { get; private set; } = [];
-        public Deck<BasePlayingCard> Deck { get; private set; }
-        public int HandsRemaining { get; private set; }
-        public int DiscardsRemaining { get; private set; }
-        public long RoundScore { get; private set; }
+        public Deck<BasePlayingCard> Deck { get; private set; } = new Deck<BasePlayingCard>([]);
+        public int HandLimit { get; private set; } = 8;
+        private int HandsRemaining { get; set; } = 3;
+        private int DiscardsRemaining { get; set; } = 3;
+        private long RoundScore { get; set; } = 0;
 
         private GameEngine GameEngine { get; set; }
+        public GameState(GameEngine gameEngine)
+        {
+            GameEngine = gameEngine;
+            // TODO: Use Strategies to customize game state initialization for different decks
+            // Initialize standard deck
+            char[] suits = { '♦', '♣', '♥', '♠' };
+            for (int i = 2; i < 15; i++)
+            {
+                string rankString = i switch
+                {
+                    11 => "J",
+                    12 => "Q",
+                    13 => "K",
+                    14 => "A",
+                    _ => i.ToString()
+                };
+                for (int j = 0; j < 4; j++)
+                {
+                    Deck.AddCard(new PlayingCard(i, suits[j], rankString: rankString, isFaceUp: true, isSelectable: true), false);
+                }
+            }
+            Deck.Shuffle();
+        }
+
         public GameState(GameEngine gameEngine, GameState gameState)
         {
             GameEngine = gameEngine;
@@ -23,6 +48,57 @@ namespace BlazorCardGame.Domain.Models
             RoundScore = gameState.RoundScore;
         }
 
+        public void AddRoundScore(long handScore)
+        {
+            RoundScore += handScore;
+        }
+
+        public void SetRoundScore(long value)
+        {
+            RoundScore = value;
+        }
+
+        public void SetHandCards(List<BasePlayingCard> handCards)
+        {
+            HandCards = handCards;
+        }
+
+        public void DecrementHandsRemaining()
+        {
+            HandsRemaining--;
+        }
+
+        public long GetHandScore()
+        {
+            List<BasePlayingCard> selectedCards = HandCards.Where(c => ((IPlayingCard) c).IsSelected()).ToList();
+            PokerLogic.HandCategory selectedCardsCategory = PokerLogic.GetHandCategory<BasePlayingCard>(HandCards.Where(c => ((IPlayingCard) c).IsSelected()).ToList());
+            // Determine points: sum of scoring cards' value and base category points
+            long points = HandScores.handBasePoints.GetValueOrDefault<PokerLogic.HandCategory, int>(selectedCardsCategory, 0);
+            // Scoring cards are cards for which their exclusion would change the hand category
+            // unless the hand category is high card, in which case score only the highest card
+            if (selectedCardsCategory != PokerLogic.HandCategory.HighCard)
+            {
+                points += HandCards.Where(
+                    c => selectedCardsCategory != PokerLogic.GetHandCategory(
+                        selectedCards.Where(
+                            c2 => !c2.Equals(c)
+                        ).ToList<IPlayingCard>()
+                    )
+                ).Sum(c => c.GetPoints());
+            }
+            else
+            {
+                points += HandCards.Where(c => ((IPlayingCard) c).IsSelected()).Max(c => c.GetPoints());
+            }
+            return points;
+        }
+
+        public long GetHandMultiplier()
+        {
+            return HandScores.handBaseMultiplier.GetValueOrDefault<PokerLogic.HandCategory, int>(PokerLogic.GetHandCategory<BasePlayingCard>(HandCards.Where(c => ((IPlayingCard) c).IsSelected()).ToList()), 0);
+        }
+
+        // Memento pattern
         public void Restore(Memento memento)
         {
             Jokers = memento.GameState.Jokers;
