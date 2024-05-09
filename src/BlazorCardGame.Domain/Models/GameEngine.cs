25a1b386d8bfde5d404ba28c83ef0a235290e2f0
coreybriscoe <coreybriscoe@gmail.com>
2024-05-08 23:23:46 -0400

    feat: Add GameEngine constructor, draw strategy, execute method

--- src/BlazorCardGame.Domain/Models/GameEngine.cs
+++ src/BlazorCardGame.Domain/Models/GameEngine.cs
@@ -1,12 +1,31 @@
+using BlazorCardGame.Domain.Patterns.Command;
+using BlazorCardGame.Domain.Patterns.Strategy.DrawStrategy;
 namespace BlazorCardGame.Domain.Models
 {
     public class GameEngine
     {
-        public GameState GameState { private get; set; }
+        public GameState GameState { get; set; }
+        public CommandHistory CommandHistory { get; set; } = new CommandHistory();
+        public IDrawStrategy<BasePlayingCard> HandDrawStrategy { get; set; }
 
+        public GameEngine()
+        {
+            GameState = new GameState(this);
+            HandDrawStrategy = new DefaultDrawStrategy<BasePlayingCard>(GameState.Deck);
+            // Draw cards
+            GameState.SetHandCards(HandDrawStrategy.DrawCards(GameState.HandLimit));
+        }
+
+        // Memento pattern
         public GameState.Memento CreateSnapshot()
         {
             return new GameState.Memento(this, GameState);   
+        }
+
+        public void ExecuteCommand(ICommand command)
+        {
+            command.Execute();
+            CommandHistory.Push(command);
         }
     }
 }
