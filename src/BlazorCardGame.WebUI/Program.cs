6c19f953fdb7c55c10ae222dfa23104975ba8cd9
coreybriscoe <coreybriscoe@gmail.com>
2024-05-08 23:27:46 -0400

    feat: Add GameEngine to DI container

--- src/BlazorCardGame.WebUI/Program.cs
+++ src/BlazorCardGame.WebUI/Program.cs
@@ -3,6 +3,7 @@ global using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorCardGame.WebUI;
+using BlazorCardGame.Domain.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluxor(options => options.ScanAssemblies(typeof(Program).Assembly));
+builder.Services.AddScoped<GameEngine>();
 
 await builder.Build().RunAsync();
